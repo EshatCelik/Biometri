546452c207982fb21495d2f2d44022c7ae3da396
Eshat Çelik <eshat.celik@gateway.com.tr>
2021-06-07 11:08:27 +0300

    Apiden gelen json veri nasıl objeye dönüşür

--- WebApi/Controllers/UserController.cs
+++ WebApi/Controllers/UserController.cs
@@ -1,9 +1,15 @@
 ﻿using Business.Abstract;
+using Entity.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace WebApi.Controllers
@@ -20,13 +26,41 @@ namespace WebApi.Controllers
         }
 
 
-        [HttpGet("get-all")]
+        [HttpGet("get-all/{id}")]
         
-        public IActionResult getAll()
-        {
-          var list=  _userService.GetAll();
-            return Ok(list);
+        public IActionResult GetAll(string id)
+       {
+
+
+            User demographicData = new User();
+
+            //----------
+
+            var client = new RestClient("Gelecek apideki url" + id);
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Key", "ValueDeğeri");
+
+            IRestResponse response = client.Execute(request);
+
+            var content = response.Content; // raw content as string
+
+
+            //var strData = await client.Content.ReadAsStringAsync();
+            var jsonObject = JsonConvert.DeserializeObject<JObject>(response.Content);
+
+            demographicData.Name = jsonObject["data"]["fatherName"].ToString();
+            demographicData.LastName = jsonObject["data"]["surname"].ToString();
+            demographicData.BirthDate = jsonObject["data"]["birthdate"].ToString();
+            demographicData.Email = jsonObject["data"]["email"].ToString();
+
+            //dynamic json = JsonConvert.DeserializeObject(content);
+
+            return Ok(demographicData);
+
+
+
 
         }
+
     }
 }
